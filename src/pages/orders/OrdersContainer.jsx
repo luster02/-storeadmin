fce21ca771891cc32b711a934a0a6d3972dc1dcc
luster02 <bombolti_02@hotmail.com>
2020-10-23 16:20:09 -0500

    [ADD]: orders page

---
+++ src/pages/orders/OrdersContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import OrdersContent from './OrdersContent'
+
+const OrdersContainer = () => {
+    return <OrdersContent />
+}
+
+export default OrdersContainer
---
+++ src/pages/orders/OrdersContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { PageHeader } from 'antd';
+
+const OrdersContent = () => {
+    return (
+        <div>
+            <PageHeader
+                title="Orders"
+            // subTitle="This is a subtitle"
+            />
+        </div>
+    )
+}
+
+export default OrdersContent
