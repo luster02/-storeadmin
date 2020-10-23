ea64dd739f6bc4179f39bca209cec00b2f6830e4
luster02 <bombolti_02@hotmail.com>
2020-10-23 16:22:30 -0500

    [ADD]: providers and routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from 'antd';
+import { Navigation } from './routes/main'
+import { SideNav } from './components/SideNav'
+import { useAuth } from './context/AuthContext'
+import { AppProvider } from './context/AppContext'
+import 'antd/dist/antd.css';
+import './App.css'
 
 function App() {
+  const { isAuth } = useAuth()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppProvider>
+      <Layout>
+        {isAuth && <SideNav />}
+        <Layout>
+          <Layout.Content className="content">
+            <Navigation />
+          </Layout.Content>
+        </Layout>
+      </Layout>
+    </AppProvider>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom'
+import { ApolloProvider } from '@apollo/client'
+import { AuthProvider } from './context/AuthContext'
+import { client } from './global/apollo'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
