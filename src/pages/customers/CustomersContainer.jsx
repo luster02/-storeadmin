5fdeca124d04a2f0558dc9d5931fa75a6e1f73d5
luster02 <bombolti_02@hotmail.com>
2020-10-23 16:19:46 -0500

    [ADD]: cstomers page

---
+++ src/pages/customers/CustomersContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import CustomersContent from './CustomersContent'
+
+const CustomersContainer = () => {
+    return <CustomersContent />
+}
+
+export default CustomersContainer
---
+++ src/pages/customers/CustomersContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { PageHeader } from 'antd';
+
+const CustomersContent = () => {
+    return (
+        <div>
+            <PageHeader
+                title="Customers"
+                // subTitle="This is a subtitle"
+            />
+        </div>
+    )
+}
+
+export default CustomersContent
