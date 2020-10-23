8b4d7053df6d0449e48e817b26168d9fc264c0e2
luster02 <bombolti_02@hotmail.com>
2020-10-23 16:19:31 -0500

    [ADD]: home page

---
+++ src/pages/home/HomeContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { PageHeader } from 'antd';
+
+const HomePage = () => {
+    return (
+        <div>
+            <PageHeader
+                title="Home"
+            // subTitle="This is a subtitle"
+            />
+        </div>
+    )
+}
+
+export default HomePage
